a8ffcabc580f22a468c71fe75a14ab98d6eaab2d
Lidia Yoshino <lidiayoshino@gmail.com>
2024-07-21 11:34:08 -0300

    create: hurt sound on death

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var audio_stream_player = $AudioStreamPlayer
 
 func _on_body_entered(body):
 	print("You died!")
 	Engine.time_scale = 0.5
+	audio_stream_player.playing = true
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 

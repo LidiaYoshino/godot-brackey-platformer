83596939c435847855d91aed984708979d2ac776
LidiaYoshino <lidiayoshino@gmail.com>
2024-07-26 17:59:47 -0300

    fix: make door_type a export enum to change door appearance

--- scripts/finish_line.gd
+++ scripts/finish_line.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+@export_enum("grass", "sand", "pumpkin", "ice", "home") var door_type: String = "grass"
+
 @onready var animation_player = $AnimationPlayer
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	print(str(door_type))
+	animated_sprite_2d.animation = door_type
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
